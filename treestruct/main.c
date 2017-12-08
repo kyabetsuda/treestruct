e9046201ef0bcf39cc6f290341fa329f4a845d42
津田準 <TsudaJun@tsudajun-no-MacBook-Air.local>
2017-12-08 23:28:31 +0900

    Initial Commit

---
+++ treestruct/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  treestruct
+//
+//  Created by 津田準 on 2017/12/08.
+//  Copyright © 2017 津田準. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
